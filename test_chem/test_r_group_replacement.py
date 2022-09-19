60b6d2c7cd1bc6dd3f27389d9628fb98aa28e922
Me <david@cozchemix.co.uk>
2022-09-19 09:14:14 +0100

    Tests for: Parent ID column added to analogues table. Count of replacements added to input table.

--- test_chem/test_r_group_replacement.py
+++ test_chem/test_r_group_replacement.py
@@ -24,79 +24,96 @@ class ScriptTest(TestCase):
         rgr = RGroupReplacement()
         response = run_script(file_in, rgr)
         self.assertTrue(response)
+        self.assertEqual(len(response.outputTables[0].columns), 3)
+        self.assertEqual(len(response.outputTables[0].columns[1].values), 710)
+        self.assertEqual(len(response.outputTables[0].columns[2].values), 710)
+        self.assertEqual(len(response.outputColumns[0].values), 5)
+        self.assertEqual(response.outputColumns[0].values[0], 7)
+        self.assertEqual(response.outputColumns[0].values[1], 98)
 
     def test_script_smarts_core2(self) -> None:
         file_in = Path(__file__).parent / 'resources' / 'test_r_group_replacement1.json'
         rgr = RGroupReplacement()
         response = run_script(file_in, rgr)
         self.assertTrue(response)
-        print(f'number of columns : {len(response.outputTables[0].columns)}')
-        print(f'number of prods : {len(response.outputTables[0].columns[1].values)}')
-        self.assertEqual(len(response.outputTables[0].columns), 2)
+        self.assertEqual(len(response.outputTables[0].columns), 3)
         self.assertEqual(len(response.outputTables[0].columns[1].values), 42)
-        mols = column_to_molecules(response.outputTables[0].columns[1])
+        self.assertEqual(len(response.outputTables[0].columns[2].values), 42)
+        mols = column_to_molecules(response.outputTables[0].columns[2])
         self.assertEqual(Chem.MolToSmiles(mols[0]), 'N#Cc1cc(S(=O)(=O)Nc2ccco2)ccc1Oc1ccccc1-c1ccccc1')
+        self.assertEqual(len(response.outputColumns[0].values), 100)
+        self.assertEqual(response.outputColumns[0].values[0], 3)
+        self.assertEqual(response.outputColumns[0].values[1], 0)
 
     def test_script_molfile_core(self) -> None:
         file_in = Path(__file__).parent / 'resources' / 'test_r_group_replacement2.json'
         rgr = RGroupReplacement()
         response = run_script(file_in, rgr)
         self.assertTrue(response)
-        print(f'number of columns : {len(response.outputTables[0].columns)}')
-        print(f'number of prods : {len(response.outputTables[0].columns[1].values)}')
-        self.assertEqual(len(response.outputTables[0].columns), 2)
-        mols = column_to_molecules(response.outputTables[0].columns[1])
+        self.assertEqual(len(response.outputTables[0].columns), 3)
+        mols = column_to_molecules(response.outputTables[0].columns[2])
         self.assertEqual(len(response.outputTables[0].columns[1].values), 42)
+        self.assertEqual(len(response.outputTables[0].columns[2].values), 42)
         self.assertEqual(Chem.MolToSmiles(mols[0]), 'N#Cc1cc(S(=O)(=O)Nc2ccco2)ccc1Oc1ccccc1-c1ccccc1')
-
+        self.assertEqual(len(response.outputColumns[0].values), 100)
+        self.assertEqual(response.outputColumns[0].values[0], 3)
+        self.assertEqual(response.outputColumns[0].values[1], 0)
 
     def test_2_subs_on_atom(self) -> None:
         mols = [Chem.MolFromSmiles('Cl[C@H](OC)c1nc(O)c[nH]1')]
+        ids = ['mol1']
         core_query = Chem.MolFromSmarts('Cc1nccn1')
-        analogue_table = replace_rgroups(mols, core_query, True, False,
+        analogue_table, _ = replace_rgroups(mols, ids, core_query, True, False,
                                          'Test')
-        analogues = column_to_molecules(analogue_table.columns[1])
+        analogues = column_to_molecules(analogue_table.columns[2])
         self.assertEqual(len(analogues), 125)
-        analogue_table = replace_rgroups(mols, core_query, False, True,
+        analogue_table, _ = replace_rgroups(mols, ids, core_query, False, True,
                                          'Test')
-        analogues = column_to_molecules(analogue_table.columns[1])
+        analogues = column_to_molecules(analogue_table.columns[2])
         self.assertEqual(len(analogues), 504)
 
     def test_duplicate_removal(self) -> None:
         mols = [Chem.MolFromSmiles('Clc1ccccc1Cl')]
+        ids = ['mol1']
         core_query = Chem.MolFromSmarts('c1ccccc1')
-        analogue_table = replace_rgroups(mols, core_query, True, False,
+        analogue_table, _ = replace_rgroups(mols, ids, core_query, True, False,
                                          'Test')
-        analogues = column_to_molecules(analogue_table.columns[1])
+        analogues = column_to_molecules(analogue_table.columns[2])
         self.assertEqual(len(analogues), 15)
 
     def test_multiple_matches(self) -> None:
         mols = [Chem.MolFromSmiles('OC(=O)c1ccccc1Oc1ccc(N)cc1')]
+        ids = ['mol1']
         core_query = Chem.MolFromSmarts('c1ccccc1')
-        analogue_table = replace_rgroups(mols, core_query, True, False,
+        analogue_table, _ = replace_rgroups(mols, ids, core_query, True, False,
                                          'Test')
-        analogues = column_to_molecules(analogue_table.columns[1])
+        analogues = column_to_molecules(analogue_table.columns[2])
         self.assertEqual(len(analogues), 10)
 
     def test_dismiss_bidentate(self) -> None:
         mols = [Chem.MolFromSmiles('c1ccc2c(c1)COC2')]
+        ids = ['mol1']
         core_query = Chem.MolFromSmarts('c1ccccc1')
-        analogue_table = replace_rgroups(mols, core_query, True, False,
-                                         'Test')
-        # we should just get the same molecule back again
-        analogues = column_to_molecules(analogue_table.columns[1])
-        self.assertEqual(len(analogues), 1)
-        self.assertEqual(Chem.MolToSmiles(analogues[0]), 'c1ccc2c(c1)COC2')
+        analogue_table, analogue_counts = replace_rgroups(mols, ids, core_query,
+                                                          True, False, 'Test')
+        # we should just get an empty table
+        analogues = column_to_molecules(analogue_table.columns[2])
+        self.assertEqual(len(analogues), 0)
+        self.assertEqual(len(analogue_counts), 1)
+        self.assertEqual(analogue_counts[0], 0)
 
     def test_do_nothing(self) -> None:
         smis = ['[nH]1c(C)nc(F)c1', '[nH]1c(C)nc(F)c1Cl', '[nH]1c(C)nc(O)c1',
                 '[nH]1c(CO)nc(F)c1c1ccc(Cl)cc1']
         mols = [Chem.MolFromSmiles(s) for s in smis]
+        ids = ['mol1', 'mol2', 'mol3', 'mol4']
         core_query = Chem.MolFromSmarts('c1nccn1')
-        analogue_table = replace_rgroups(mols, core_query, False, False,
-                                         'Test')
-        analogues = column_to_molecules(analogue_table.columns[1])
+        analogue_table, analogue_counts = replace_rgroups(mols, ids, core_query,
+                                                          False, False, 'Test')
+        analogues = column_to_molecules(analogue_table.columns[2])
         self.assertEqual(len(analogues), 0)
+        self.assertEqual(len(analogue_counts), 4)
+        self.assertEqual(len([ac for ac in analogue_counts if ac == 0]), 4)
 
 if __name__ == '__main__':
     main()
