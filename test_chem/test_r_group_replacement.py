af20af4b4384154e4442b857a104e298ef3776dd
Me <david@cozchemix.co.uk>
2022-09-19 16:44:39 +0100

    Test for: Don't colour R Groups that didn't have replacements.

--- test_chem/test_r_group_replacement.py
+++ test_chem/test_r_group_replacement.py
@@ -31,6 +31,12 @@ class ScriptTest(TestCase):
         self.assertEqual(len(response.outputColumns[0].values), 5)
         self.assertEqual(response.outputColumns[0].values[0], 7)
         self.assertEqual(response.outputColumns[0].values[1], 98)
+        mols = column_to_molecules(response.outputTables[0].columns[2])
+        print(mols[0].GetProp('Renderer_Highlight'))
+        for at in mols[0].GetAtoms():
+            print(f'{at.GetIdx()+1}  {at.GetAtomicNum()} {at.GetIsAromatic()}')
+        for b in mols[0].GetBonds():
+            print(f'{b.GetIdx()} : {b.GetBeginAtomIdx()+1} -> {b.GetEndAtomIdx()+1} : {b.GetBondType()}')
 
     def test_script_smarts_core2(self) -> None:
         file_in = Path(__file__).parent / 'resources' / 'test_r_group_replacement1.json'
@@ -135,11 +141,24 @@ class ScriptTest(TestCase):
         mols = [Chem.MolFromSmiles(s) for s in smis]
         ids = ['mol1', 'mol2']
         core_query = Chem.MolFromSmarts('Nc1ccccc1')
-        analogue_table, analogue_counts = replace_rgroups(mols, ids, DataType.STRING, core_query,
-                                                          True, False, 'Test')
+        analogue_table, _ = replace_rgroups(mols, ids, DataType.STRING, core_query,
+                                            True, False, 'Test')
         analogues = column_to_molecules(analogue_table.columns[2])
         an_smi = [Chem.MolToSmiles(an) for an in analogues]
         self.assertNotIn(smis[1], an_smi)
+
+    def test_dont_colour_no_change_r_groups(self) -> None:
+        # because there's no replacement for FCCCCCCCCCCCCO, it should
+        # not be coloured.
+        smis = ['FCCCCCCCCCCCCOc1ccc(OCC)cc1N']
+        mols = [Chem.MolFromSmiles(s) for s in smis]
+        ids = ['mol1']
+        core_query = Chem.MolFromSmarts('Nc1ccccc1')
+        analogue_table, _ = replace_rgroups(mols, ids, DataType.STRING, core_query,
+                                            True, False, 'Test')
+        analogues = column_to_molecules(analogue_table.columns[2])
+        render_string = 'COLOR #0000ff\nATOMS 1 2 3 4 5 6 7\nBONDS 1 2 3 4 7 5 6\nCOLOR #ff0000\nATOMS 22 23\nBONDS 21 22'
+        self.assertEqual(analogues[0].GetProp('Renderer_Highlight'), render_string)
 
 
 if __name__ == '__main__':
